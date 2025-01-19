880a460c9272d912a108837951fe712b4be90522
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-01-19 15:00:02 -0500

    Checking

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
-      <h1>Vedanshu Patel</h1>
+      <h1>Vedanshu Daxesh Patel</h1>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
