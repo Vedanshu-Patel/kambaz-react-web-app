fad50abe358e0e3f37b1e7a419127ce506b645a8
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-01-31 22:10:41 -0500

    Version 2

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -1,5 +1,5 @@
 import { FormControl } from "react-bootstrap";
-import { Form, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 export default function Signin() {
   return (
     <div id="wd-signin-screen">
