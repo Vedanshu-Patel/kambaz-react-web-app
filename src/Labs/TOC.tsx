ba9ce7a6fd69e12fbb5aa0182558841e0ab4d879
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-02-05 14:27:19 -0500

    Final version 1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
        <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
      </Nav.Item>
      <Nav.Item>
-       <Nav.Link href="https://github.com/Vedanshu-Patel?tab=repositories">My GitHub</Nav.Link>
+       <Nav.Link href="https://github.com/Vedanshu-Patel?tab=repositories" id="wd-github">My GitHub</Nav.Link>
      </Nav.Item>
    </Nav>
 
