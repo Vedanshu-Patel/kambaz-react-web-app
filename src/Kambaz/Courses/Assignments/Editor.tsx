914cce1cdff392249756dc958ff802fd304d644b
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-02-05 14:24:39 -0500

    Final version 1

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -1,151 +1,148 @@
+import { Button, Col, Form, FormCheck, FormControl, FormGroup, FormLabel, Row } from "react-bootstrap";
+
 export default function AssignmentEditor() {
   return (
-    <div id="wd-assignments-editor">
-      <label htmlFor="wd-name"><h3>Assignment Name</h3></label>
-      <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-      <textarea id="wd-description">
-        The assignment is available online Submit a link to the landing page of
-      </textarea>
-      <br />
-      <table>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-points">Points</label>
-          </td>
-          <td>
-            <input id="wd-points" value={100} />
-          </td>
-        </tr><br></br>
-        {/* Complete on your own */}
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-group">Assignment Group</label>
-          </td>
-          <td>
-            <select id="wd-group">
+    <div id="wd-assignments-editor" >
+
+      <Form>
+        <FormGroup>
+          <FormLabel htmlFor="wd-name">
+            <h3>Assignment Name</h3>
+          </FormLabel>
+          <FormControl type="text" id="wd-name" value="A1">
+
+          </FormControl>
+        </FormGroup>
+        <FormGroup><br />
+          <FormControl as="textarea" id="wd-description" rows={3}>
+            This assignment is
+            Submit a link to the landing page of  online our Web application running on Netlify.
+
+            The landing page should be the Kambaz application with a link to the Lab exercises.
+
+            Lab 1 should be the landing page of the Lab exercises and should include the following:
+
+            Your full name and section
+            Links to each of the lab assignments
+            Link to the Kambaz application
+            Links to all relevant source code repositories
+            The Kambaz application should include a link to navigate back to the landing page.
+          </FormControl>
+        </FormGroup><br />
+        <FormGroup as={Row}>
+          <Col md="3" className="text-end">
+            <FormLabel htmlFor="wd-points" className="mb-0">
+              Points
+            </FormLabel>
+          </Col>
+          <Col md="9">
+            <FormControl type="text" id="wd-points" value={100} />
+          </Col>
+        </FormGroup><br />
+        <FormGroup as={Row}>
+          <Col md="3" className="text-end">
+            <FormLabel htmlFor="wd-group" className="mb-0">
+              Assignment Group
+            </FormLabel>
+          </Col>
+          <Col md="9">
+            <Form.Select id="wd-group">
               <option value={"ASSIGNMENTS"}>ASSIGNMENTS</option>
               <option value={"PROJECT"}>PROJECT</option>
               <option value={"QUIZZES"}>QUIZZES</option>
               <option value={"EXAMS"}>EXAMS</option>
-            </select>
-          </td>
-        </tr><br></br>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-display-grade-as">Display Grade as</label>
-          </td>
-          <td>
-            <select id="wd-display-grade-as">
+            </Form.Select>
+          </Col>
+        </FormGroup><br />
+        <FormGroup as={Row}>
+          <Col md="3" className="text-end">
+            <FormLabel htmlFor="wd-display-grade-as" className="mb-0">
+              Display Grade as
+            </FormLabel>
+          </Col>
+          <Col md="9">
+            <Form.Select id="wd-display-grade-as">
               <option value={"Percentage"}>Percentage</option>
               <option value={"Marks"}>Marks</option>
               <option value={"Percentile"}>Percentile</option>
-            </select>
-          </td>
-        </tr><br></br>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-submission-type">Submission Type</label>
-          </td>
-          <td>
-            <select id="wd-submission-type">
-              <option value={"Online"}>Online</option>
-              <option value={"Offline"}>Offline</option>
-              <option value={"Hybrid"}>Hybrid</option>
-            </select>
-          </td>
-        </tr><br></br>
-        <tr>
-          <td align="right" valign="top">
-            
-          </td>
-          <td>
-            <label>Online Entry Options</label><br></br>
-            <input id="wd-text-entry" type="checkbox"/>
-            <label htmlFor="wd-text-entry">Text Entry</label>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            
-          </td>
-          <td>
-            <input id="wd-website-url" type="checkbox"/>
-            <label htmlFor="wd-website-url">Website URL</label>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            
-          </td>
-          <td>
-            <input id="wd-media-recordings" type="checkbox"/>
-            <label htmlFor="wd-media-recordings">Media Recordings</label>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            
-          </td>
-          <td>
-            <input id="wd-student-annotation" type="checkbox"/>
-            <label htmlFor="wd-student-annotation">Student Annotation</label>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            
-          </td>
-          <td>
-            <input id="wd-student-annotation" type="checkbox"/>
-            <label htmlFor="wd-student-annotation">Student Annotation</label>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            
-          </td>
-          <td>
-            <input id="wd-file-upload" type="checkbox"/>
-            <label htmlFor="wd-file-upload">File Uploads</label>
-          </td>
-        </tr><br></br>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-assign-to">Assign</label>
-          </td>
-          <td>
-            <label htmlFor="wd-assign-to">Assign to</label><br></br>
-            <input id="wd-assign-to" value={"Everyone"} /><br></br> 
-          </td>
-        </tr><br></br>
-        <tr>
-          <td align="right" valign="top">
-            
-          </td>
-          <td>
-            <label htmlFor="wd-due-date">Due</label><br></br> 
-            <input id="wd-due-date" type="date" value={"2024-05-13"} />
-          </td>
-        </tr><br></br>
-        <tr>
-          <td align="right" valign="top">
-            
-          </td>
-          <td>
-            <label htmlFor="wd-available-from">Available from</label><br></br> 
-            <input id="wd-available-from" type="date" value={"2024-05-13"} />
-          </td>
-          <td>
-            <label htmlFor="wd-available-until">Until</label><br></br> 
-            <input id="wd-available-until" type="date" value={"2024-05-13"} />
-          </td>
-        </tr>
-      </table>
-      <hr></hr>
-      <div style={{ textAlign: "right" }}>
-        <button type="button" >Cancel</button><span> </span>
-        <button type="button">Save</button>
-      </div>
+            </Form.Select>
+          </Col>
+        </FormGroup><br />
+        <FormGroup as={Row}>
+          <Col md="3" className="text-end">
+            <FormLabel htmlFor="wd-submission-type" className="mb-0">
+              Submission Type
+            </FormLabel>
+          </Col>
+          <Col md="9" >
+            <div className="border px-2">
+              <Form.Select id="wd-submission-type" className="mt-2">
+                <option value={"Online"}>Online</option>
+                <option value={"Offline"}>Offline</option>
+                <option value={"Hybrid"}>Hybrid</option>
+              </Form.Select><br />
+              <FormGroup>
+                <span><strong>Online Entry Options</strong></span><br /><br />
+                <FormCheck type="checkbox" id="wd-text-entry" label="Text Entry" /><br />
+                <FormCheck type="checkbox" id="wd-website-url" label="Website URL" defaultChecked /><br />
+                <FormCheck type="checkbox" id="wd-media-recordings" label="Media Recordings" /><br />
+                <FormCheck type="checkbox" id="wd-student-annotation" label="Student Annotation" /><br />
+                <FormCheck type="checkbox" id="wd-file-upload" label="File Uploads" /><br />
+              </FormGroup>
+            </div>
+          </Col>
+        </FormGroup><br />
+        <FormGroup as={Row}>
+          <Col md="3" className="text-end">
+            <FormLabel htmlFor="wd-assign-to" className="mb-0">
+              Assign
+            </FormLabel>
+          </Col>
+          <Col md="9" >
+            <div className="border px-2 pb-4">
+              <br />
+              <FormLabel htmlFor="wd-assign-to" className="mb-0">
+                <strong>Assign to</strong>
+              </FormLabel>
+              <FormControl type="text" id="wd-assign-to" className="mt-2" value={"Everyone"}>
+
+              </FormControl><br />
+              <FormLabel htmlFor="wd-due-date" className="mb-0">
+                <strong>Due</strong>
+              </FormLabel>
+              <FormControl type="date" value={"2025-05-25"} id="wd-due-date"></FormControl>
+              <br />
+              <Row>
+                <Col>
+                  <FormLabel htmlFor="wd-available-from" className="mb-0">
+                    <strong>Available From</strong>
+                  </FormLabel><br />
+                  <FormControl type="date" value={"2025-05-25"} id="wd-available-from"></FormControl>
+                </Col>
+                <Col>
+                  <FormLabel htmlFor="wd-available-until" className="mb-0">
+                    <strong>Until</strong>
+                  </FormLabel>
+                  <FormControl type="date" value={"2025-05-25"} id="wd-available-until"></FormControl>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </FormGroup><br />
+        <hr></hr>
+        <FormGroup as={Row} className="mb-3">
+          <Col sm={{ span: 10, offset: 2 }}>
+            <div className="d-flex justify-content-end">
+              <Button variant="secondary" className="me-2">
+                Cancel
+              </Button>
+              <Button variant="danger">
+                Save
+              </Button>
+            </div>
+          </Col>
+        </FormGroup>
+      </Form>
     </div>
   );
 }
--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,32 +1,118 @@
+import { Col, Container, ListGroup, Row } from "react-bootstrap";
+import AssignmentControls from "./AssignmentControls";
+import { BsGripVertical } from "react-icons/bs";
+import LessonControlButtons from "../Modules/LessonControlButtons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import AssignmentControlButtons from "./AssignmentControlButtons";
+import { MdEditDocument } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import { BiEdit } from "react-icons/bi";
+
 export default function Assignments() {
-    return (
-      <div id="wd-assignments">
-        <input placeholder="Search for Assignments"
-               id="wd-search-assignment" />
-        <button id="wd-add-assignment-group">+ Group</button>
-        <button id="wd-add-assignment">+ Assignment</button>
-        <h3 id="wd-assignments-title">
-          ASSIGNMENTS 40% of Total <button>+</button> </h3>
-        <ul id="wd-assignment-list">
-          <li className="wd-assignment-list-item">
-            <a href="#/Kambaz/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-              A1 - ENV + HTML
-            </a> 
-            <br></br>Multiple Modules | <b>Not available until</b> April 12 at 10:00am | <b>Due</b> April 9 at 11:59pm | 100 pts</li>
-            <li className="wd-assignment-list-item">
-            <a href="#/Kambaz/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-              A2 - Node + Express
-            </a> 
-            <br></br>Multiple Modules | <b>Not available until</b> April 12 at 10:00am | <b>Due</b> April 27 at 11:59pm | 100 pts</li>
-            <li className="wd-assignment-list-item">
-            <a href="#/Kambaz/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-              A3 - Mongoose + MongoDB
-            </a> 
-            <br></br>Multiple Modules | <b>Not available until</b> April 12 at 10:00am | <b>Due</b> May 17 at 11:59pm | 100 pts</li>
-        </ul>
-      </div>
-  );}
-  
+  return (
+    <div id="wd-assignments">
+      <AssignmentControls /><br /><br /><br /><br />
+      <ListGroup className="rounded-0" id="wd-modules">
+        <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary1" id="wd-assignments-title"><BsGripVertical className="me-2 fs-3" /><FontAwesomeIcon icon={faCaretDown} /> ASSIGNMENTS<AssignmentControlButtons />
+          </div>
+          <ListGroup className="wd-lesson rounded-0" id="wd-assignment-list">
+            <ListGroup.Item className="wd-lesson p-3 ps-1 wd-assignment-list-item">
+              <div className="d-flex">
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <BsGripVertical className="me-2 fs-3" />
+                </span>
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <MdEditDocument className="me-2 fs-3" style={{ color: '#008000' }} />
+                </span>
+                <div className="position-relative flex-grow-1">
+                  <LessonControlButtons />
+                  <a className="wd-assignment-link text-black link-underline link-underline-opacity-0" href="#/Kambaz/Courses/1234/Assignments/123">
+                    <b>A1</b>
+                  </a>
+                  <div>
+                    <p><text className="text-danger">Multiple Modules</text> | <b>Not Available until</b> July 4 at 10:20am | <b>Due</b> July 14 at 11:59pm | 100 pts</p>
+                  </div>
+                </div></div>
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1 wd-assignment-list-item">
+              <div className="d-flex">
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <BsGripVertical className="me-2 fs-3" />
+                </span>
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <MdEditDocument className="me-2 fs-3" style={{ color: '#008000' }} />
+                </span>
+                <div className="position-relative flex-grow-1">
+                  <LessonControlButtons />
+                  <a className="wd-assignment-link text-black link-underline link-underline-opacity-0" href="#/Kambaz/Courses/1234/Assignments/123">
+                    <b>A2</b>
+                  </a>
+                  <div>
+                    <p><text className="text-danger">Multiple Modules</text> | <b>Not Available until</b> Feb 19 at 9:00am | <b>Due</b> Feb 24 at 11:59pm | 100 pts</p>
+                  </div>
+                </div></div>
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1 wd-assignment-list-item">
+              <div className="d-flex">
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <BsGripVertical className="me-2 fs-3" />
+                </span>
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <MdEditDocument className="me-2 fs-3" style={{ color: '#008000' }} />
+                </span>
+                <div className="position-relative flex-grow-1">
+                  <LessonControlButtons />
+                  <a className="wd-assignment-link text-black link-underline link-underline-opacity-0" href="#/Kambaz/Courses/1234/Assignments/123">
+                    <b>A3</b>
+                  </a>
+                  <div>
+                    <p><text className="text-danger">Multiple Modules</text> | <b>Not Available until</b> April 7 at 8:00am | <b>Due</b> April 20 at 11:59pm | 100 pts</p>
+                  </div>
+                </div></div>
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1 wd-assignment-list-item">
+              <div className="d-flex">
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <BsGripVertical className="me-2 fs-3" />
+                </span>
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <MdEditDocument className="me-2 fs-3" style={{ color: '#008000' }} />
+                </span>
+                <div className="position-relative flex-grow-1">
+                  <LessonControlButtons />
+                  <a className="wd-assignment-link text-black link-underline link-underline-opacity-0" href="#/Kambaz/Courses/1234/Assignments/123">
+                    <b>A4</b>
+                  </a>
+                  <div>
+                    <p><text className="text-danger">Multiple Modules</text> | <b>Not Available until</b> Sept 12 at 11:00am | <b>Due</b> Sept 13 at 11:59pm | 100 pts</p>
+                  </div>
+                </div></div>
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1 wd-assignment-list-item">
+              <div className="d-flex">
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <BsGripVertical className="me-2 fs-3" />
+                </span>
+                <span style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <MdEditDocument className="me-2 fs-3" style={{ color: '#008000' }} />
+                </span>
+                <div className="position-relative flex-grow-1">
+                  <LessonControlButtons />
+                  <a className="wd-assignment-link text-black link-underline link-underline-opacity-0" href="#/Kambaz/Courses/1234/Assignments/123">
+                    <b>A5</b>
+                  </a>
+                  <div>
+                    <p><text className="text-danger">Multiple Modules</text> | <b>Not Available until</b> Sept 10 at 11:00am | <b>Due</b> Sept 20 at 11:59pm | 100 pts</p>
+                  </div>
+                </div></div>
+            </ListGroup.Item>
+          </ListGroup>
+        </ListGroup.Item>
+
+      </ListGroup>
+
+    </div>
+  );
+}
