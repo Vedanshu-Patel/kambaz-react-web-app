4e13bf19cc7936a2cfd98f7c234213864240a99d
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-04-02 18:14:22 -0400

    cloud

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+console.log("REMOTE_SERVER",REMOTE_SERVER);
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 export const createCourse = async (course: any) => {
   const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
