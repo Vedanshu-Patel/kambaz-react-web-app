1588c73223a131e259037e03570d49da4176e6ea
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-04-08 14:06:12 -0400

    Modules

--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -3,14 +3,14 @@ const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 const axiosWithCredentials = axios.create({ withCredentials: true });
 export const createModuleForCourse = async (courseId: string, module: any) => {
-  const response = await axios.post(`${COURSES_API}/${courseId}/modules`,
+  const response = await axiosWithCredentials.post(`${COURSES_API}/${courseId}/modules`,
     module
   );
   return response.data;
 };
 
 export const findModulesForCourse = async (courseId: string) => {
-  const response = await axios
+  const response = await axiosWithCredentials
     .get(`${COURSES_API}/${courseId}/modules`);
   return response.data;
 };
