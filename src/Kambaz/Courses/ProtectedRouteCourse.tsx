857bf78e76c8a53a1d4d8ad86eaa7a973e9172e1
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-04-09 10:30:26 -0400

    ProtectedCourseRoute issue

--- src/Kambaz/Courses/ProtectedRouteCourse.tsx
+++ src/Kambaz/Courses/ProtectedRouteCourse.tsx
@@ -2,27 +2,16 @@ import { useSelector } from "react-redux";
 // import { enrollments } from "../Database";
 import { useNavigate, useParams } from "react-router-dom";
 
-export default function ProtectedRouteCourse({children}:{ children: any }){
-    const { cid } = useParams();
-    const { enrollments } = useSelector((state: any) => state.enrollmentReducer);
-    const { currentUser } = useSelector((state: any) => state.accountReducer);
-    const navigate = useNavigate();
-    const isEnrolled = (cId: any) => {
-        return enrollments.some((enrollment: any) =>enrollment.course === cId && currentUser._id  ===  enrollment.user);
-    }
-        
-        if(isEnrolled(cid)){
-            return children;
-        }
-        else{
-            navigate(`/Kambaz/Dashboard`)
-        }
-    
-}
 // export default function ProtectedRouteCourse({children}:{ children: any }){
+//     const { cid } = useParams();
+//     const { enrollments } = useSelector((state: any) => state.enrollmentReducer);
 //     const { currentUser } = useSelector((state: any) => state.accountReducer);
 //     const navigate = useNavigate();
-//         if(currentUser){
+//     const isEnrolled = (cId: any) => {
+//         return enrollments.some((enrollment: any) =>enrollment.course === cId && currentUser._id  ===  enrollment.user);
+//     }
+        
+//         if(isEnrolled(cid)){
 //             return children;
 //         }
 //         else{
@@ -30,3 +19,14 @@ export default function ProtectedRouteCourse({children}:{ children: any }){
 //         }
     
 // }
+export default function ProtectedRouteCourse({children}:{ children: any }){
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const navigate = useNavigate();
+        if(currentUser){
+            return children;
+        }
+        else{
+            navigate(`/Kambaz/Dashboard`)
+        }
+    
+}
